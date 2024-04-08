3ad4fc9b80efeae7a8b0d1845a1d992bbca60a74
Nicolas C. <github@liliwol.fr>
2024-04-08 16:48:18 +0200

    Upate

---
+++ NextBus/assets/script.js
@@ -0,0 +1,41 @@
+axios.get(
+    'BusStops.json',
+    {
+        headers: {
+            "Cache-Control": "no-cache",
+            'Content-Type': 'application/json',
+        }
+    }
+)
+    .then(function (response) {
+        // handle success
+        console.clear();
+
+        console.log(response.data);
+
+        response.data.forEach(busStop => {
+            // Create a marker and add it to the map
+            let marker = L.marker([busStop.Latitude, busStop.Longitude]).addTo(map);
+            marker.bindPopup(`<b>${busStop.Name}</b><br><a href="#" class="nextBus" data-bus-stop="${busStop.Id}">Afficher les prochains bus:</a>`);
+        });
+
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .finally(function () {
+        // always executed
+    });
+
+
+// Create a map object and set the view to the default location
+var map = L.map('map').setView([46.160328, -1.151139], 16);
+
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # OpenData La rochelle
 
+https://www.agglo-larochelle.fr/fu/data/#api-transports-endpoints
 
 ## Carte des arrêts de bus de la ville de La Rochelle
 
 Cette carte représente les arrêts de bus de la ville de La Rochelle. Elle a été réalisée à partir des données ouvertes de la ville de La Rochelle.
 BusStops/index.html
+
+![](readme_docs/01019c2b.png)
